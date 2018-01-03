7f200ff42d18057ed5d7343af8441e3e68c354d6
Reza Parang <rezaparang@Rezas-MacBook-Pro.local>
2018-01-02 18:17:20 -0800

    bing routes and verification

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+
+  def bing_verify
+	data = '<?xml version="1.0"?><users><user>0820DD2B1700372D911D7738B5510DE8</user></users>'
+    render xml: data
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # Support old routes! List these first, otherwise the entries controller
   # looks for the slug value and fails
   get '/entries/1-backdoor-roth-ira-ultimate-fidelity-step-by-step-guide', to: redirect('/backdoor-roth-ira-ultimate-fidelity-step-by-step-guide', status: 301)
+  
+  # Verify for Microsoft's Bing search
+  get '/BingSiteAuth.xml' => 'pages#bing_verify'
 
   root :to => "entries#index"
 
